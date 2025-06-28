59b5413154578245f42b8898f9c3a66ca62ca1a6
DavidG159 <gaspardavid235@gmail.com>
2025-06-27 21:10:13 -0600

    timer test part1 added a few things

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
+//VARIABLES
 const timer = document.querySelector('.timer');
+let counter = 0;
+
+
+//FUNCTIONS
+setTimeout(greeting, 1000);
+
+function greeting() {
+    console.log("Hello!");
+    console.log(counter++);
+}
+
+//LISTENERS
