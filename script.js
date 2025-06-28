c57f68b62b6667e9b5430f0460db136ddbfcbd97
DavidG159 <gaspardavid235@gmail.com>
2025-06-28 12:48:48 -0600

    added pauseTime function + finding out a bug with the start button

--- script.js
+++ script.js
@@ -10,22 +10,24 @@ let timestart;
 
 
 //FUNCTIONS
-
-
-
 function countDown() {
-
     if (counter === 1) {
         clearInterval(timestart);
         console.log("STOP")
     }
-    counter--;
-    timer.textContent = counter;
+    counter--;                  //decrements the counter
+    timer.textContent = counter;//updating the timer in the DOM 
+}
+
+function pauseTime() {
+    clearInterval(timestart);
+    console.log("Pause button is clicked!");
 }
 
 
-
 //LISTENERS
-startbtn.addEventListener('click', function () {
+startbtn.addEventListener('click', function () { //when the start button is clicked the timer starts counting down to every 1 second
     timestart = setInterval(countDown, 1000);
 });
+
+
