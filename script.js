a4098a5d560044d926d28d5be0bdf574c3274dc9
DavidG159 <gaspardavid235@gmail.com>
2025-06-27 21:22:30 -0600

    timer test part1 made it working but still figuring out stuff

--- script.js
+++ script.js
@@ -2,15 +2,24 @@
 
 //VARIABLES
 const timer = document.querySelector('.timer');
-let counter = 0;
+let counter = 10;
 
 
 //FUNCTIONS
-setTimeout(greeting, 1000);
 
-function greeting() {
-    console.log("Hello!");
-    console.log(counter++);
+setInterval(countDown, 1000);
+
+function countDown() {
+
+    if (counter === 0) {
+        clearInterval();
+        console.log("STOP")
+    }
+    counter--;
+    timer.textContent = counter;
 }
+
+
+
 
 //LISTENERS
